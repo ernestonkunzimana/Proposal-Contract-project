247f4c4e0696ec4666a4806260d8b9cc32afe913
Ernest Nkunzimana <50280217+ernestonkunzimana@users.noreply.github.com>
2025-01-22 16:02:35 +0200

    Update proposal.sol
    I have added title field in the Proposal struct and updates the create function to accept a title.

--- proposal.sol
+++ proposal.sol
@@ -1,97 +1,75 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-
 contract ProposalContract {
     // ****************** Data ***********************
-
-
-    //Owner
+    // Owner
     address owner;
-
 
     uint256 private counter;
 
-
     struct Proposal {
+        string title; // Title of the proposal
         string description; // Description of the proposal
         uint256 approve; // Number of approve votes
         uint256 reject; // Number of reject votes
         uint256 pass; // Number of pass votes
         uint256 total_vote_to_end; // When the total votes in the proposal reaches this limit, proposal ends
-        bool current_state; // This shows the current state of the proposal, meaning whether if passes of fails
-        bool is_active; // This shows if others can vote to our contract
+        bool current_state; // This shows the current state of the proposal, meaning whether it passes or fails
+        bool is_active; // This shows if others can vote on our contract
     }
-
 
     mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
 
+    address[] private voted_addresses;
 
-    address[] private voted_addresses; 
-
-
-    //constructor
+    // Constructor
     constructor() {
         owner = msg.sender;
         voted_addresses.push(msg.sender);
     }
-
 
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
-
     modifier active() {
         require(proposal_history[counter].is_active == true);
         _;
     }
 
-
     modifier newVoter(address _address) {
-        require(!isVoted(_address), "Address has not voted yet");
+        require(!isVoted(_address), "Address has already voted");
         _;
     }
 
-
     // ****************** Execute Functions ***********************
-
-
-
 
     function setOwner(address new_owner) external onlyOwner {
         owner = new_owner;
     }
 
-
-    function create(string calldata _description, uint256 _total_vote_to_end) external onlyOwner {
+    function create(string calldata _title, string calldata _description, uint256 _total_vote_to_end) external onlyOwner {
         counter += 1;
-        proposal_history[counter] = Proposal(_description, 0, 0, 0, _total_vote_to_end, false, true);
+        proposal_history[counter] = Proposal(_title, _description, 0, 0, 0, _total_vote_to_end, false, true);
     }
-    
 
-
-    function vote(uint8 choice) external active newVoter(msg.sender){
+    function vote(uint8 choice) external active newVoter(msg.sender) {
         Proposal storage proposal = proposal_history[counter];
         uint256 total_vote = proposal.approve + proposal.reject + proposal.pass;
 
-
         voted_addresses.push(msg.sender);
-
 
         if (choice == 1) {
             proposal.approve += 1;
-            proposal.current_state = calculateCurrentState();
         } else if (choice == 2) {
             proposal.reject += 1;
-            proposal.current_state = calculateCurrentState();
         } else if (choice == 0) {
             proposal.pass += 1;
-            proposal.current_state = calculateCurrentState();
         }
 
+        proposal.current_state = calculateCurrentState();
 
         if ((proposal.total_vote_to_end - total_vote == 1) && (choice == 1 || choice == 2 || choice == 0)) {
             proposal.is_active = false;
@@ -99,29 +77,22 @@ contract ProposalContract {
         }
     }
 
-
     function terminateProposal() external onlyOwner active {
         proposal_history[counter].is_active = false;
     }
 
-
-
-
-    function calculateCurrentState() private view returns(bool) {
+    function calculateCurrentState() private view returns (bool) {
         Proposal storage proposal = proposal_history[counter];
-
 
         uint256 approve = proposal.approve;
         uint256 reject = proposal.reject;
         uint256 pass = proposal.pass;
-        
-        if (proposal.pass %2 == 1) {
+
+        if (proposal.pass % 2 == 1) {
             pass += 1;
         }
 
-
         pass = pass / 2;
-
 
         if (approve > reject + pass) {
             return true;
@@ -130,11 +101,7 @@ contract ProposalContract {
         }
     }
 
-
-
-
     // ****************** Query Functions ***********************
-
 
     function isVoted(address _address) public view returns (bool) {
         for (uint i = 0; i < voted_addresses.length; i++) {
@@ -145,15 +112,11 @@ contract ProposalContract {
         return false;
     }
 
-
-
-
-    function getCurrentProposal() external view returns(Proposal memory) {
+    function getCurrentProposal() external view returns (Proposal memory) {
         return proposal_history[counter];
     }
 
-
-    function getProposal(uint256 number) external view returns(Proposal memory) {
+    function getProposal(uint256 number) external view returns (Proposal memory) {
         return proposal_history[number];
     }
 }
