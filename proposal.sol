4fe700d18f688a6781f30d8680f5e7baa3908c90
Ernest Nkunzimana <50280217+ernestonkunzimana@users.noreply.github.com>
2025-01-22 15:06:54 +0200

    Update proposal.sol
    This contract is a comprehensive decentralized voting system for managing proposals, designed to ensure transparency and fairness by allowing various voting options and ensuring only eligible users can vote. Its flexible structure allows for easy addition of new features and custom logic for determining proposal outcomes.

--- proposal.sol
+++ proposal.sol
@@ -14,8 +14,9 @@ contract ProposalContract {
         uint approve; // Number of approve votes
         uint reject; // Number of reject votes
         uint pass; // Number of pass votes
+        uint hold; // Number of hold votes
         uint total_vote_to_end; // When the total votes in the proposal reaches this limit, proposal ends
-        bool current_state; // This shows the current state of the proposal, meaning whether it passes or fails
+        string current_state; // This shows the current state of the proposal, whether it passes, fails, or is held
         bool is_active; // This shows if others can vote to our contract
         uint createdAt; // Timestamp when the proposal was created
         mapping(address => uint) voters; // Tracks who has voted and their vote type
@@ -38,8 +39,8 @@ contract ProposalContract {
         _;
     }
 
-    modifier notVoted(uint proposalId) {
-        require(proposal_history[proposalId].voters[msg.sender] == 0, "You have already voted");
+    modifier newVoter(address _address) {
+        require(!isVoted(_address), "Address has already voted");
         _;
     }
 
@@ -64,18 +65,18 @@ contract ProposalContract {
         newProposal.description = _description;
         newProposal.total_vote_to_end = _total_vote_to_end;
         newProposal.is_active = true;
-        newProposal.current_state = false;
+        newProposal.current_state = "Pending";
         newProposal.createdAt = block.timestamp;
 
         emit ProposalCreated(counter, _title, _description, _total_vote_to_end);
     }
 
-    function vote(uint proposalId, uint8 choice) external active(proposalId) notVoted(proposalId) {
+    function vote(uint proposalId, uint8 choice) external active(proposalId) newVoter(msg.sender) {
         Proposal storage proposal = proposal_history[proposalId];
-        uint256 total_vote = proposal.approve + proposal.reject + proposal.pass;
+        uint256 total_vote = proposal.approve + proposal.reject + proposal.pass + proposal.hold;
 
         // Validate the choice
-        require(choice == 0 || choice == 1 || choice == 2, "Invalid vote choice");
+        require(choice == 0 || choice == 1 || choice == 2 || choice == 3, "Invalid vote choice");
 
         // Apply the vote
         if (choice == 1) {
@@ -84,6 +85,8 @@ contract ProposalContract {
             proposal.reject += 1;
         } else if (choice == 0) {
             proposal.pass += 1;
+        } else if (choice == 3) {
+            proposal.hold += 1;
         }
 
         proposal.voters[msg.sender] = choice;
@@ -95,17 +98,46 @@ contract ProposalContract {
         // Check if the proposal should be closed
         if (total_vote + 1 >= proposal.total_vote_to_end) {
             proposal.is_active = false;
+            voted_addresses = new address[](1);  // Reset the voted_addresses array
+            voted_addresses[0] = owner;
         }
     }
 
-    function calculateCurrentState(uint proposalId) internal view returns (bool) {
-        Proposal storage proposal = proposal_history[proposalId];
-        return proposal.approve > proposal.reject;
+    function isVoted(address _address) internal view returns (bool) {
+        for (uint i = 0; i < voted_addresses.length; i++) {
+            if (voted_addresses[i] == _address) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    function getProposal(uint _proposalId) public view returns (string memory, string memory, uint, uint, uint, uint, bool, bool, uint) {
+    function calculateCurrentState(uint proposalId) private view returns (string memory) {
+        Proposal storage proposal = proposal_history[proposalId];
+
+        uint256 approve = proposal.approve;
+        uint256 reject = proposal.reject;
+        uint256 pass = proposal.pass;
+        uint256 hold = proposal.hold;
+
+        if (proposal.pass % 2 == 1) {
+            pass += 1;
+        }
+
+        pass = pass / 2;
+
+        if (approve > reject + pass && approve < hold) {
+            return "Held";
+        } else if (approve > reject + pass) {
+            return "Approved";
+        } else {
+            return "Rejected";
+        }
+    }
+
+    function getProposal(uint _proposalId) public view returns (string memory, string memory, uint, uint, uint, uint, uint, string memory, bool, uint) {
         Proposal storage proposal = proposals[_proposalId];
-        return (proposal.title, proposal.description, proposal.approve, proposal.reject, proposal.pass, proposal.total_vote_to_end, proposal.current_state, proposal.is_active, proposal.createdAt);
+        return (proposal.title, proposal.description, proposal.approve, proposal.reject, proposal.pass, proposal.hold, proposal.total_vote_to_end, proposal.current_state, proposal.is_active, proposal.createdAt);
     }
 
     function getProposalHistory() public view returns (uint[] memory _ids) {
